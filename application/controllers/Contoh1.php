d0c71957fd264680cd1c9a9f30d4cf0b532e0f0d
MochammadTaufikJuliandi <taufik.juliandi4@gmail.com>
2022-03-22 17:15:40 +0700

    master

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Halo Selamat Datang di Web Saya<br>";
+        echo "Saya Mochammad Taufik Juliandi dari kelas 15.4A.28<br>";
+        echo "Salam Kenal!!!";
+    }
+}
+?>
